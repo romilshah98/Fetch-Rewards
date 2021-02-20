2f2a0a0481956074a89d399dbb3416624a51944b
Romil Shah <romilshah6462@gmail.com>
2021-02-19 17:45:32 -0800

    instructions and comments added

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Fetch-Rewards
+# Fetch Rewards Web Service
+---
+
+## How to run this web service
+***
+1. Download the zip file and open in any code editor.
+2. From terminal go to the same folder and run below commands.
+    * npm i
+    * node ./server.js
+3. npm i - will install all the dependancy of this project and the 2nd command will host the server on port 4000.
+4. Use any API testing tools such as Postman to check the web service.
+5. Use "/points" endpoint to get available points of all the payers (GET request).
+6. Use "/addTransaction" to add new transaction (POST request). _Note_ - In the request body send data as JSON.
+7. Use "/spend" to add spend points (POST request). _Note_ - In the request body send data as JSON.
+
  
--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const { json } = require("express");
+// Imports
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+
+// Using express
+const app = express();
 app.use(bodyParser.json());
+
+// Port where server is hosted
 const port = 4000;
 
+// Defining our in memory transactions
 const transactions = [];
+let availablePoints = 0;
 
+// Listen on our port
 app.listen(port, () => {
   console.log(`API Server started on port ${port}`);
 });
 
+// Get points of all payers
 app.get("/points", (req, res) => {
   let points = new Map();
+  // Here we are traversing through all the transactions to calculate total points of each player
+  // and store it in map.
   transactions.forEach((transaction) => {
     if (points.has(transaction["payer"])) {
       const currPoints = points.get(transaction["payer"]);
@@ -24,53 +34,76 @@ app.get("/points", (req, res) => {
   res.status(200).json(Object.fromEntries(points));
 });
 
+// Add new transaction
 app.post("/addTransaction", (req, res) => {
   const transactionData = req.body;
-  const date = new Date(Date.now());
-  const timeStamp = date.toISOString();
-  const transaction = { ...transactionData, timestamp: timeStamp };
-  transactions.push(transaction);
-  res.status(200).send("Transaction added successfully!");
+
+  if (!transactionData["payer"] || !transactionData["points"]) {
+    res.status(200).send("Please provide Payer and Points");
+  } else {
+    const date = new Date(Date.now());
+    const timeStamp = date.toISOString();
+    availablePoints += transactionData.points;
+    const transaction = { ...transactionData, timestamp: timeStamp };
+    transactions.push(transaction);
+    res.status(200).send("Transaction added successfully!");
+  }
 });
 
+// Spend points
 app.post("/spend", (req, res) => {
   const points = req.body.points;
+
   try {
     response = spendPoints(points);
     res.status(200).send(response);
   } catch (e) {
-    res.status(200).send("Something went wrong!");
+    res.status(200).send(e.message);
   }
 });
 
+// Spend Points helper function
 const spendPoints = (pointsToSpend) => {
   let currPoints = pointsToSpend;
+
+  // Throwing error if we try to spend more points then available
+  if (currPoints > availablePoints) {
+    throw new Error("You cannot spend more than " + availablePoints.toString() + " points.");
+  }
+
   let spendies = [];
-  const l = transactions.length;
-  for (let index = 0; index < l; index++) {
-    console.log(currPoints);
+  const transactionLength = transactions.length;
+
+  //  Traversing list of transaction until we spend all points.
+  for (let index = 0; index < transactionLength; index++) {
     if (currPoints == 0) {
       break;
     }
+
     const transaction = transactions.shift();
     let payer = transaction["payer"];
     let points = transaction["points"];
+
+    // If current transaction's point is less than points we want to spend then
+    // we spend all points of the current transaction.
     if (points <= currPoints) {
       currPoints -= points;
+      availablePoints -= points;
       spendies.push({ payer: payer, points: -points });
-    } else {
+    } // else we only spend points that we need to.
+    else {
       spendies.push({ payer: payer, points: -currPoints });
+      availablePoints -= currPoints;
       const date = new Date(Date.now());
       const timeStamp = date.toISOString();
+      // Since we don't spend all the points, we have to add new transaction with the remaining point
       transactions.push({
         payer: payer,
         points: points - currPoints,
         timeStamp: timeStamp,
       });
-      console.log(transactions);
       currPoints = 0;
     }
   }
-  console.log(spendies);
   return [...spendies];
 };
